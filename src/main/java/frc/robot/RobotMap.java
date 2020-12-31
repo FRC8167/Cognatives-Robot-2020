1ddb6ad9f1c86135f88c712874574bdbaa38144d
tjf801 <fuscofamily0@gmail.com>
2020-12-31 15:50:02 -0500

    Update RobotMap.java

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -16,48 +16,46 @@ package frc.robot;
 public class RobotMap {
 	//I have no idea why we have parentheses around the numbers, but whatever
 	//Stop judging me it looks cool -Conner
-	//TODO: remove parenthesis, conner is not here -Tyler
-	//TODO: make these all final, maybe even into separate enums or sm
+	//remove parenthesis, conner is not here -Tyler
 	
 	//Joystick
-	public static int joystickPort = (0);
+	public static final int joystickPort = 0;
 	
 	//Motors
-	public static int frontLeftPort = (1);
-	public static int rearLeftPort = (2);
-	public static int frontRightPort = (3);
-	public static int rearRightPort = (4);
+	public static final int frontLeftPort = 1;
+	public static final int rearLeftPort = 2;
+	public static final int frontRightPort = 3;
+	public static final int rearRightPort = 4;
 	
-	public static int wheelMotorPort = (9);
-	public static int colorServoPort = (5);
+	public static final int colorServoPort = 5;
+	public static final int wheelMotorPort = 9;
+	
+	//USB ports
+	public static final int cameraPort = 0;
 	
 	//Servos
-	public static int dumpActuatorPort = (6);
-	public static final int ultrasonicPort = (0);
-	
-	public static final int cameraPort = 0;
-	public static int cameraServoPort = (7);
+	public static final int dumpActuatorPort = 6;
+	public static final int cameraServoPort = 7;
 	
 	//Buttons
-	public static int dumpButtonNumber = (1);
-	public static int loadButtonNumber = (3); //Added by nick to make it goto the load point
-	public static int reverseButtonNumber = (4);
-	public static int ultrasonicSensorButtonNumber = (5); //TODO: do we need this? didnt the UltS break?
-	public static int wheelClockwiseButtonNumber = (7);
-	public static int wheelAntiClockwiseButtonNumber = (8);
-	public static int colorDetectButtonNumber = (9);
-	//public static int servoButtonNumber = (5);
-	public static int turn90DegreesButtonNumber = (11);
-	public static int servoCameraButtonNumber = (10);
+	public static final int dumpButtonNumber = 1;
+	public static final int loadButtonNumber = 3; //Added by nick to make it goto the load point
+	public static final int reverseButtonNumber = 4;
+	public static final int ultrasonicSensorButtonNumber = 5; //TODO: do we need this? didnt the UltS break?
+	public static final int wheelClockwiseButtonNumber = 7;
+	public static final int wheelAntiClockwiseButtonNumber = 8;
+	public static final int colorDetectButtonNumber = 9;
+	public static final int servoCameraButtonNumber = 10;
+	public static final int turn90DegreesButtonNumber = 11;
 	
 	//Speed Values for QuickTurnCommand
-	public static double turnLeftValue = (-.75);
-	public static double turnRightValue = (.75);
-	public static double turn180Value = (.76);
+	public static final double turnLeftValue = -.75;
+	public static final double turnRightValue = .75;
+	public static final double turn180Value = .76;
 	
-	//TODO: what is this?
-	public static final double kValueToCM = 9.77;
-	public static final double kValueToInches = .125;
+	//idk what this is but its never used
+	//public static final double kValueToCM = 9.77;
+	//public static final double kValueToInches = .125;
 }
 
 
